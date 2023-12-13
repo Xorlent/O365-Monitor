206ebe373ed06ea1f9da6574d792cfc0495dea97
Xorlent <94985297+Xorlent@users.noreply.github.com>
2023-12-13 10:48:17 -0800

    Add files via upload

---
+++ Get-DormantO365Users.ps1
@@ -0,0 +1,40 @@
+﻿$ConfigFile = '.\O365Monitor-Config.xml'
+$ConfigParams = [xml](get-content $ConfigFile)
+
+$LogFile = '.\O365Montior-Dormant.csv'
+
+# Initialize configuration variables from config xml file
+$TenantID = $ConfigParams.o365app.tenantid.value
+$APPObjectID = $ConfigParams.o365app.appid.value
+
+Connect-MgGraph -ClientID $APPObjectID -TenantId $TenantID -CertificateName "CN=O365Monitor" -ErrorAction SilentlyContinue -Errorvariable ConnectionError | Out-Null
+
+if($ConnectionError -ne $null)
+    {
+    Write-Host "$ConnectionError" -Foregroundcolor Red
+    Exit
+    }
+
+if (Test-Path -Path $LogFile -PathType Leaf){
+    rm $LogFile
+}
+
+$Headers = '"Last Sign-In","Display Name","User Principal Name"'
+$UserList = Get-MgUser -Filter 'accountEnabled eq true' -All
+
+Add-Content $LogFile $Headers
+
+foreach ($User in $UserList)
+{
+    $UserID = $User.Id
+    $UserName = $User.DisplayName
+    $UserEmail = $User.UserPrincipalName
+    $LastLogin = Get-MgUser -UserId $UserID -Property 'SignInActivity'
+
+    if($LastLogin.SignInActivity.LastSignInDateTime -lt (Get-Date).AddDays(-45)){
+        $Entry = '"' + $LastLogin.SignInActivity.LastSignInDateTime + '","' + $UserName + '","' + $UserEmail + '"'
+        Add-Content $LogFile $Entry
+    }
+}
+
+Disconnect-MgGraph
---
+++ Get-ExpiringO365AppRegistrations.ps1
@@ -0,0 +1,81 @@
+﻿$ConfigFile = '.\O365Monitor-Config.xml'
+$ConfigParams = [xml](get-content $ConfigFile)
+
+$LogFile = '.\O365Montior.csv'
+
+# Initialize configuration variables from config xml file
+$TenantID = $ConfigParams.o365app.tenantid.value
+$APPObjectID = $ConfigParams.o365app.appid.value
+
+Connect-MgGraph -ClientID $APPObjectID -TenantId $TenantID -CertificateName "CN=O365Monitor" -ErrorAction SilentlyContinue -Errorvariable ConnectionError | Out-Null
+
+if($ConnectionError -ne $null)
+    {
+    Write-Host "$ConnectionError" -Foregroundcolor Red
+    Exit
+    }
+
+if (Test-Path -Path $LogFile -PathType Leaf){
+    rm $LogFile
+}
+
+$Now = Get-Date
+$Applications = Get-MgApplication -all
+$Logs = @()
+$NotificationFlag = 0
+
+foreach ($App in $Applications) {
+    $AppName = $App.DisplayName
+    $AppID   = $App.Id
+    $ApplID  = $App.AppId
+
+    $AppCreds = Get-MgApplication -ApplicationId $AppID | Select-Object PasswordCredentials, KeyCredentials
+
+    $Secrets = $AppCreds.PasswordCredentials
+    $Certs   = $AppCreds.KeyCredentials
+
+    foreach ($Secret in $Secrets) {
+        $StartDate  = $Secret.StartDateTime
+        $EndDate    = $Secret.EndDateTime
+        $SecretName = $Secret.DisplayName
+
+        $RemainingDaysCount = ($EndDate - $Now).Days
+
+        if ($RemainingDaysCount -le 45) {
+            $Logs += [PSCustomObject]@{
+                'App Name'        = $AppName
+                'App ID'          = $ApplID
+                'Secret Name'            = $SecretName
+                'Secret Expiration'      = $EndDate
+                'Certificate Name'       = $Null
+                'Certificate Expiration' = $Null
+            }
+            $NotificationFlag++
+        }
+    }
+
+    foreach ($Cert in $Certs) {
+        $StartDate = $Cert.StartDateTime
+        $EndDate   = $Cert.EndDateTime
+        $CertName  = $Cert.DisplayName
+
+        $RemainingDaysCount = ($EndDate - $Now).Days
+
+        if ($RemainingDaysCount -le 45) {
+            $Logs += [PSCustomObject]@{
+                'App Name'        = $AppName
+                'App ID'          = $ApplID
+                'Secret Name'            = $Null
+                'Certificate Name'       = $CertName
+                'Certificate Expiration' = $EndDate
+            }
+            $NotificationFlag++
+        }
+    }
+}
+
+if($NotificationFlag){
+    $Logs | Out-File -FilePath $LogFile
+    }
+
+Disconnect-MgGraph
